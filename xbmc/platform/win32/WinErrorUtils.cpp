801efa11ab001db0abd071b4c5409d5238df8476
CrystalP <crystalp@kodi.tv>
2024-10-15 19:38:55 -0400

    [Windows] Synchronize dxerr.h/dxerr.cpp with upstream
    - minimized the differences with upstream
    - kept the changes required for successful build on desktop and uwp
    - kept the typo fixes
    - removed codes and descriptions for DirectInput and Direct3D9
    - DirectSound messages moved to WinErrorUtils

--- xbmc/platform/win32/WinErrorUtils.cpp
+++ xbmc/platform/win32/WinErrorUtils.cpp
@@ -25,6 +25,10 @@ using namespace KODI::PLATFORM::WINDOWS;
   case err: \
     return #err
 
+#define ERRTODESC(err, desc) \
+  case err: \
+    return desc
+
 namespace
 {
 static const std::string GetDSoundErrorString(HRESULT hr)
@@ -57,6 +61,52 @@ static const std::string GetDSoundErrorString(HRESULT hr)
     ERRTOSTR(DSERR_BADSENDBUFFERGUID);
     ERRTOSTR(DSERR_OBJECTNOTFOUND);
     ERRTOSTR(DSERR_FXUNAVAILABLE);
+    default:
+      break;
+  }
+#endif
+  return {};
+}
+
+static const std::string GetDSoundErrorDescription(HRESULT hr)
+{
+#ifdef TARGET_WINDOWS_DESKTOP
+  switch (hr)
+  {
+    ERRTODESC(DS_OK, "");
+    ERRTODESC(DS_NO_VIRTUALIZATION,
+              "The call succeeded, but we had to substitute the 3D algorithm");
+    ERRTODESC(DSERR_ALLOCATED, "The call failed because resources (such as a priority level) were "
+                               "already being used by another caller");
+    ERRTODESC(DSERR_CONTROLUNAVAIL,
+              "The control (vol, pan, etc.) requested by the caller is not available");
+    ERRTODESC(DSERR_INVALIDPARAM, "DSERR_INVALIDPARAM");
+    ERRTODESC(DSERR_INVALIDCALL, "This call is not valid for the current state of this object");
+    ERRTODESC(DSERR_GENERIC, "DSERR_GENERIC");
+    ERRTODESC(DSERR_PRIOLEVELNEEDED,
+              "The caller does not have the priority level required for the function to succeed");
+    ERRTODESC(DSERR_OUTOFMEMORY, "Not enough free memory is available to complete the operation");
+    ERRTODESC(DSERR_BADFORMAT, "The specified WAVE format is not supported");
+    ERRTODESC(DSERR_UNSUPPORTED, "DSERR_UNSUPPORTED");
+    ERRTODESC(DSERR_NODRIVER, "No sound driver is available for use");
+    ERRTODESC(DSERR_ALREADYINITIALIZED, "This object is already initialized");
+    ERRTODESC(DSERR_NOAGGREGATION, "DSERR_NOAGGREGATION");
+    ERRTODESC(DSERR_BUFFERLOST, "The buffer memory has been lost, and must be restored");
+    ERRTODESC(DSERR_OTHERAPPHASPRIO,
+              "Another app has a higher priority level, preventing this call from succeeding");
+    ERRTODESC(DSERR_UNINITIALIZED, "This object has not been initialized");
+    ERRTODESC(DSERR_NOINTERFACE, "DSERR_NOINTERFACE");
+    ERRTODESC(DSERR_ACCESSDENIED, "DSERR_ACCESSDENIED");
+    ERRTODESC(DSERR_BUFFERTOOSMALL,
+              "Tried to create a DSBCAPS_CTRLFX buffer shorter than DSBSIZE_FX_MIN milliseconds");
+    ERRTODESC(DSERR_DS8_REQUIRED,
+              "Attempt to use DirectSound 8 functionality on an older DirectSound object");
+    ERRTODESC(DSERR_SENDLOOP, "A circular loop of send effects was detected");
+    ERRTODESC(DSERR_BADSENDBUFFERGUID,
+              "The GUID specified in an audiopath file does not match a valid MIXIN buffer");
+    ERRTODESC(DSERR_OBJECTNOTFOUND,
+              "The object requested was not found (numerically equal to DMUS_E_NOT_FOUND)");
+    ERRTODESC(DSERR_FXUNAVAILABLE, "Requested effects are not available");
     default:
       break;
   }
@@ -120,7 +170,15 @@ const std::string CWinError::FormatDSoundError(HRESULT hr)
   if (!code.length())
     code = FromW(DXGetErrorStringW(hr));
 
-  return std::format("0x{:X} {}", static_cast<uint32_t>(hr), code);
+  std::string desc{GetDSoundErrorDescription(hr)};
+  if (!desc.length())
+  {
+    WCHAR buff[2048];
+    DXGetErrorDescriptionW(hr, buff, 2048);
+    desc = FromW(buff);
+  }
+
+  return std::format("0x{:X} {} ({})", static_cast<uint32_t>(hr), code, desc);
 }
 
 const std::string CWinError::FormatHRESULT(HRESULT hr)
