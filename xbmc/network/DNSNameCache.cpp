09d9341ecbaebba1c764a801d5141c6c6931c354
CrystalP <crystalp@kodi.tv>
2024-11-29 02:08:54 -0500

    code instrumented with lots of debug information

--- xbmc/network/DNSNameCache.cpp
+++ xbmc/network/DNSNameCache.cpp
@@ -54,12 +54,16 @@ bool CDNSNameCache::Lookup(const std::string& strHostName, std::string& strIpAdd
       inet_pton(AF_INET6, strHostName.c_str(), &addr6))
   {
     strIpAddress = strHostName;
+    CLog::LogF(LOGDEBUG, "strHostname is IP address {}", strIpAddress);
     return true;
   }
 
   // check if there's a custom entry or if it's already cached
   if (g_DNSCache.GetCached(strHostName, strIpAddress))
+  {
+    CLog::LogF(LOGDEBUG, "strHostname found in cache [{}] > [{}]", strHostName, strIpAddress);
     return true;
+  }
 
   // perform dns lookup
   addrinfo hints{};
@@ -68,17 +72,25 @@ bool CDNSNameCache::Lookup(const std::string& strHostName, std::string& strIpAdd
   hints.ai_family = AF_UNSPEC;
   hints.ai_socktype = SOCK_STREAM;
   hints.ai_flags |= AI_CANONNAME;
-  
+
   if (getaddrinfo(strHostName.c_str(), nullptr, &hints, &res) == 0)
   {
     strIpAddress = CNetworkBase::GetIpStr(res->ai_addr);
+    addrinfo* ptr = res->ai_next;
+    while (ptr)
+    {
+      CLog::LogF(LOGDEBUG, "getaddrinfo next: [{}]", CNetworkBase::GetIpStr(ptr->ai_addr));
+      ptr = ptr->ai_next;
+    }
+
     freeaddrinfo(res);
-    g_DNSCache.Add(strHostName, strIpAddress);
-    return true;
+    CLog::LogF(LOGDEBUG, "getaddrinfo found [{}] > [{}]", strHostName, strIpAddress);
+    //g_DNSCache.Add(strHostName, strIpAddress);
+    //return true;
   }
   
 #if defined(TARGET_WINDOWS)
-  static DWORD nameSpaces[]{NS_NETBT, NS_WINS, NS_BTH, NS_NTDS};
+  static DWORD nameSpaces[]{NS_DNS, NS_NETBT, NS_WINS, NS_NLA, NS_BTH, NS_NTDS, NS_ALL};
 
   const std::wstring wStrHostName = KODI::PLATFORM::WINDOWS::ToW(strHostName);
   ADDRINFOEXW wHints{};
@@ -96,9 +108,25 @@ bool CDNSNameCache::Lookup(const std::string& strHostName, std::string& strIpAdd
     if (status == NO_ERROR)
     {
       strIpAddress = CNetworkBase::GetIpStr(wRes->ai_addr);
+      ADDRINFOEXW* ptr = wRes->ai_next;
+      while (ptr)
+      {
+        CLog::LogF(LOGDEBUG, "GetAddrInfoExW next: [{}]", CNetworkBase::GetIpStr(ptr->ai_addr));
+        ptr = ptr->ai_next;
+      }
+
       FreeAddrInfoExW(wRes);
-      g_DNSCache.Add(strHostName, strIpAddress);
-      return true;
+      CLog::LogF(LOGDEBUG, "GetAddrInfoExW ns {} found [{}] > [{}]", ns, strHostName, strIpAddress);
+
+      if (ns == NS_ALL)
+      {
+        g_DNSCache.Add(strHostName, strIpAddress);
+        return true;
+      }
+    }
+    else
+    {
+      CLog::Log(LOGDEBUG, "Lookup failed with namespace {}: error code {}", ns, WSAGetLastError());
     }
   }
 #endif
